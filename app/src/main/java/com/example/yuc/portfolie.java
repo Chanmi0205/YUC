f7f906a16857818aa56800d42b1d682074c4f547
Chanmi0205 <chanmi1155@naver.com>
2022-01-12 22:19:26 +0900

    여울컴 포트폴리오 추가 하기(미완성)

--- app/src/main/java/com/example/yuc/portfolie.java
+++ app/src/main/java/com/example/yuc/portfolie.java
@@ -2,13 +2,47 @@ package com.example.yuc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class portfolie extends AppCompatActivity {
+
+    private TextView app;
+    private TextView web;
+    private TextView game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_portfolie);
+
+        game = findViewById(R.id.game);
+        game.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(portfolie.this, game.class);
+                startActivity(intent);
+            }
+        });
+
+        web = findViewById(R.id.web);
+        web.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(portfolie.this, wep.class);
+                startActivity(intent);
+            }
+        });
+
+        app = findViewById(R.id.app);
+        app.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(portfolie.this, app.class);
+                startActivity(intent);
+            }
+        });
     }
 }
