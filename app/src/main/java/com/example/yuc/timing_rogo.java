a98ac275869a23f0af91badf225e90df2a5d9c97
Chanmi0205 <chanmi1155@naver.com>
2022-01-15 22:34:09 +0900

    여울컴 포트폴리오 추가 하기(미완성)

---
+++ app/src/main/java/com/example/yuc/timing_rogo.java
@@ -0,0 +1,14 @@
+package com.example.yuc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class timing_rogo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timing_rogo);
+    }
+}
